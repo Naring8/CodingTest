69aeb0eb6f9368485e3e554183830a82709c13e7
unknown <skell01@naver.com>
2023-10-07 17:57:41 +0900

    BOJ1149

---
+++ BOJ1149.cpp
@@ -0,0 +1,64 @@
+// 시작 (2023-10-07  17:00)
+// 종료 (2023-10-07  17:40) - 실패
+// 종료 (2023-10-07  18:00) - 실패
+
+#include <iostream>
+#include <vector>
+
+struct RGB
+{
+	int R = 0;
+	int G = 0;
+	int B = 0;
+
+	int Sum() { return R + G + B; }
+};
+
+int main()
+{
+	int N;
+
+	std::cin >> N;
+	if (N < 2 || N > 1000)
+		return 0;
+
+	std::vector<RGB> ColorCosts;
+	int MinCost = 0;
+
+	for (int i = 0; i < N; i++)
+	{
+		int ValueR, ValueG, ValueB;
+		std::cin >> ValueR >> ValueG >> ValueB;
+
+		{
+			RGB temp;
+			temp.R = ValueR;
+			temp.G = ValueG;
+			temp.B = ValueB;
+
+			ColorCosts.push_back(temp);
+
+			MinCost += temp.Sum();
+		}
+	}
+
+	{
+		int Cost = 0;
+
+		// 첫번째가 R
+		Cost += ColorCosts[0].R;
+
+
+		// 첫번째가 G
+		Cost += ColorCosts[0].G;
+
+
+		// 첫번째가 B
+		Cost += ColorCosts[0].B;
+
+	}
+	
+
+
+	return 0;
+}

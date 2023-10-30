6a1a74fc06241abc2fdfa96f3ab940e755ca40f1
unknown <skell01@naver.com>
2023-10-30 23:03:36 +0900

    Purchase Cards
    correct

--- BOJ_11052.cpp
+++ BOJ_11052.cpp
@@ -1,7 +1,7 @@
 // Start (2023-10-30 21:55)
 // End (2023-10-30 22:35)
 
-#include <iostream>
+/*#include <iostream>
 #include <map>
 
 using namespace std;
@@ -35,6 +35,49 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	int N;
+	cin >> N;
+
+	prices[0] = 0;
+	for (int i = 1; i <= N; i++)	// input
+	{
+		int price;
+		cin >> price;
+		prices[i] = price;
+	}
+
+	cout << GetMaxPrice(N);
+
+	return 0;
+}*/
+
+//===============================================================================================
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+map<int, int> prices;	// {number, price}
+int MaxPrices[1001];
+
+int GetMaxPrice(const int MaxCount)
+{
+	int MaxPrice = prices[MaxCount];
+
+	if (!MaxCount)
+		return MaxPrice;
+
+	for (int i = 1; i <= MaxCount; i++)
+		for (int j = 1; j <= i; j++)
+			MaxPrices[i] = max(MaxPrices[i], MaxPrices[i - j] + prices[j]);
+
+	return MaxPrices[MaxCount];
+}
+
+int main()
+{
 	int N;
 	cin >> N;
 

b03e3c420aaa689caaf8a44f5f29f691b8ac2ac5
unknown <skell01@naver.com>
2023-11-05 17:59:52 +0900

    Programmers_Cash_1 (O)
    modify codes

--- Programmers_Cash_1.cpp
+++ Programmers_Cash_1.cpp
@@ -13,16 +13,19 @@ using namespace std;
 // 앞 뒤 접근이 가능 + 다른 원소에도 접근이 가능(list) / 대소문자 구분이 X -> Upper or Lower를 써야할지도?
 // 있으면 1, 없으면 5
 
-int solution(int cacheSize, vector<string> cities) 
+int solution(int cacheSize, vector<string> cities)
 {
 	int answer = 0;
 
 	list<string> cityL;
 
+	if (cacheSize == 0)
+		return cities.size() * 5;
+
 	for (auto& c : cities)
 	{
 		string city = c;
-		transform(city.begin(), city.end(), city.begin(), toupper);
+		transform(city.begin(), city.end(), city.begin(), ::toupper);
 		bool findData = false;
 
 		if (cityL.empty())	// 맨 첫번째
@@ -44,21 +47,19 @@ int solution(int cacheSize, vector<string> cities)
 				findData = true;
 				break;
 			}
-			findData = false;
 		}
 
 		if (findData)
 			continue;
-
 		else  // 같은 게 없을 때
 		{
 			cityL.push_back(city);
 			answer += 5;
-			if (cityL.size() > cacheSize)	
+			if (cityL.size() > cacheSize)
 				cityL.pop_front();
 		}
 	}
-	
+
 	return answer;
 }
 
@@ -71,5 +72,5 @@ int main()
 	cout << solution(5, { "Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome" }) << "\n";
 	cout << solution(2, { "Jeju", "Pangyo", "NewYork", "newyork" }) << "\n";
 	cout << solution(0, { "Jeju", "Pangyo", "Seoul", "NewYork", "LA" }) << "\n";
-	
+
 }
